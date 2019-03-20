82df190b1a5f0c8484bd5018b477bc478ed2296e
Rodrigo <rodrigo3mil@hotmail.com>
2019-03-20 10:11:24 -0300

    Iniciando tela de registro

--- app/src/main/java/com/app/topservices/RegistrarActivity.java
+++ app/src/main/java/com/app/topservices/RegistrarActivity.java
@@ -2,6 +2,8 @@ package com.app.topservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class RegistrarActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class RegistrarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+
+
+
     }
 }
