85aec41d7d75b4284b2c147287b0fd2219a51984
Rodrigo <rodrigo3mil@hotmail.com>
2019-03-22 22:05:31 -0300

    Edição da tela de registro com Radio Button pra selecionar o tipo de cadastro

--- app/src/main/java/com/app/topservices/RegistrarActivity.java
+++ app/src/main/java/com/app/topservices/RegistrarActivity.java
@@ -2,8 +2,13 @@ package com.app.topservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class RegistrarActivity extends AppCompatActivity {
 
@@ -12,8 +17,23 @@ public class RegistrarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
 
+       final RadioGroup group = (RadioGroup) findViewById(R.id.RadioGroup);
+        final LinearLayout Condo = (LinearLayout) findViewById(R.id.FormCondominio);
 
+        final LinearLayout Prof = (LinearLayout) findViewById(R.id.FormProfissional);
+       Condo.setVisibility(View.GONE);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(RadioGroup radioGroup, int i) {
+              if(i == R.id.RadioCondominio){
+                  Prof.setVisibility(View.INVISIBLE);
+                  Condo.setVisibility(View.VISIBLE);
+              }else if(i == R.id.RadioProfissional){
+                  Condo.setVisibility(View.GONE);
+                  Prof.setVisibility(View.VISIBLE);
+              }
 
-
+           }
+       });
     }
 }
