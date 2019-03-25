6d38e5b347d2b9ba2d1e5c447d13357fa6b91eb1
Rodrigo <rodrigo3mil@hotmail.com>
2019-03-24 22:55:10 -0300

    Função para validar o login do usuario

--- app/src/main/java/com/app/topservices/LoginActivity.java
+++ app/src/main/java/com/app/topservices/LoginActivity.java
@@ -2,17 +2,41 @@ package com.app.topservices;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import Config.ConfiguracaoFirebase;
+import Model.Profissional;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText Login;
+    private EditText Senha;
+    private Button Entrar;
+    private String userLogin;
+    private String userSenha;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Login = (EditText) findViewById(R.id.EdtLogin);
+        Senha = (EditText) findViewById(R.id.EdtSenha);
+        Entrar = (Button) findViewById(R.id.BtnLogin);
 
 
         final Button Registrar = (Button) this.findViewById(R.id.BtnCadastrar);
@@ -25,5 +49,36 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        Entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin = Login.getText().toString();
+                userSenha = Senha.getText().toString();
+                validarLogin();
+            }
+
+
+        });
+
+
+
+    }
+
+    private void validarLogin() {
+            autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+            autenticacao.signInWithEmailAndPassword(
+                    userLogin,
+                    userSenha
+            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if( task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Login efetuado com sucesso", Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Erro ao efetuar o login" , Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
     }
 }
